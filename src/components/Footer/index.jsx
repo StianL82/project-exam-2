f25a2690624fb4aeb39448bdce43839771941f4f
StianL82 <stian.lilleng@gmail.com>
2025-01-20 15:35:26 +0100

    Added Logo and BackToTop to the Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import * as S from "./index.styles";
+import { useNavigate } from 'react-router-dom';
+import * as S from './index.styles';
 
 function Footer() {
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate('/', { replace: true });
+    window.scrollTo(0, 0);
+  };
+
   return (
     <S.FooterContainer>
-      <S.FooterText>&copy; {new Date().getFullYear()} PE2 by Stian Lilleng</S.FooterText>
+      <div className="container">
+        <div className="back-to-top">
+          <a href="#top" onClick={() => window.scrollTo(0, 0)}>
+            <img
+              src="/images/icons/back-arrow.svg"
+              alt="Back to Top Arrow icon"
+            />
+            Back to top
+          </a>
+        </div>
+        <div onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
+          <S.Logo src="/images/logo.png" alt="Logo for Holidaze" />
+        </div>
+        <S.FooterText>
+          &copy; {new Date().getFullYear()} PE2 by Stian Lilleng
+        </S.FooterText>
+      </div>
     </S.FooterContainer>
   );
 }
--- src/components/Footer/index.styles.js
+++ src/components/Footer/index.styles.js
@@ -3,15 +3,58 @@ import styled from 'styled-components';
 export const FooterContainer = styled.footer`
   background-color: ${(props) => props.theme.color.beige};
   text-align: center;
-  padding: 40px;
+  padding: 40px 20px;
   margin-top: 40px;
-  left: 0;
-  bottom: 0;
   width: 100%;
   border-top: 1px solid ${(props) => props.theme.color.darkGray};
+
+  .container {
+    max-width: 1400px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .back-to-top {
+    align-self: flex-center;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    a {
+      display: flex;
+      align-items: center;
+      gap: 2px;
+      color: ${(props) => props.theme.color.darkGray};
+      text-decoration: none;
+      font-family: 'Nunito', sans-serif;
+      font-size: 16px;
+      transition: color 0.3s;
+
+      &:hover {
+        font-weight: bold;
+      }
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+      transform: rotate(90deg);
+    }
+  }
+`;
+
+export const Logo = styled.img`
+  height: auto;
+  width: 100px;
+  max-width: 100%;
+  object-fit: contain;
+  margin: 0 auto;
 `;
 
 export const FooterText = styled.p`
-  margin: 0;
+  margin: 10px 0 0;
   font-size: 14px;
 `;
