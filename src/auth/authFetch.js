1d35d1d2735b25c6ec83fb6a9686ee9b75860304
StianL82 <stian.lilleng@gmail.com>
2025-01-24 14:01:19 +0100

    Updated authFetch.js with JSDocs documentation

--- src/auth/authFetch.js
+++ src/auth/authFetch.js
@@ -1,6 +1,11 @@
 import { API_KEY } from './constants';
 import { load } from '../storage/load';
 
+/**
+ * Generates headers for API requests, including content type, authorization, and API key.
+ *
+ * @returns {Object} Headers object for API requests.
+ */
 export function headers() {
   const token = load('token');
 
@@ -11,6 +16,15 @@ export function headers() {
   };
 }
 
+/**
+ * A wrapper function for the Fetch API that includes default headers and handles errors.
+ *
+ * @async
+ * @param {string} url - The URL to fetch.
+ * @param {Object} [options={}] - Additional options for the fetch request (e.g., method, body, headers).
+ * @returns {Promise<Object>} - The parsed JSON response from the API.
+ * @throws {Error} Throws an error if the response is not ok, including status and error message.
+ */
 export async function authFetch(url, options = {}) {
   try {
     const response = await fetch(url, {
@@ -34,3 +48,4 @@ export async function authFetch(url, options = {}) {
     throw error;
   }
 }
+
