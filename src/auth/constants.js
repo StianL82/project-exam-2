f7ae4bfc8774a6710e2cafb8f9fe4d7a7c05cde8
StianL82 <stian.lilleng@gmail.com>
2025-01-24 09:52:23 +0100

    Add constants for API URLs and key

---
+++ src/auth/constants.js
@@ -0,0 +1,8 @@
+export const API_BASE_URL = 'https://v2.api.noroff.dev';
+export const API_HOLIDAZE_BASE = '/holidaze';
+export const API_AUTH_BASE = '/auth';
+
+export const API_AUTH_URL = `${API_BASE_URL}${API_AUTH_BASE}`;
+export const API_HOLIDAZE_URL = `${API_BASE_URL}${API_HOLIDAZE_BASE}`;
+
+export const API_KEY = process.env.REACT_APP_API_KEY;
