c99073821e79f6f6841434633cedfb1d792d0341
StianL82 <stian.lilleng@gmail.com>
2025-02-14 13:58:01 +0100

    Removed Scroll to top from Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,23 +4,14 @@ import * as S from './index.styles';
 
 function Footer() {
   const navigate = useNavigate();
+
   const handleLogoClick = () => {
     navigate('/', { replace: true });
-    window.scrollTo(0, 0);
   };
 
   return (
     <S.FooterContainer>
       <div className="container">
-        <div className="back-to-top">
-          <a href="#top" onClick={() => window.scrollTo(0, 0)}>
-            <img
-              src="/images/icons/back-arrow.svg"
-              alt="Back to Top Arrow icon"
-            />
-            Back to top
-          </a>
-        </div>
         <div onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
           <S.Logo src="/images/logo.png" alt="Logo for Holidaze" />
         </div>
