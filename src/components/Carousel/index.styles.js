33154c5c5ad71a4f0a7c149a52f2a0a672020436
StianL82 <stian.lilleng@gmail.com>
2025-02-21 12:06:40 +0100

    Update index.styles.js

--- src/components/Carousel/index.styles.js
+++ src/components/Carousel/index.styles.js
@@ -9,7 +9,7 @@ export const StyledCarousel = styled.div`
     border-radius: 10px;
     object-fit: cover;
     width: 100%;
-    height: 300px;
+    height: 350x;
   }
 
   .carousel-control-prev-icon,
